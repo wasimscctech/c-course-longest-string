408befa6835828d90796bf698b21b4c34f354d51
wasimscctech <98011392+wasimscctech@users.noreply.github.com>
2022-05-05 20:02:11 +0530

    Update longest_string.c

--- longest_string.c
+++ longest_string.c
@@ -4,20 +4,18 @@
 
 int main(int argc, char *argv[])
 {
+    char *longest_str = argv[1];
     if (argc == 1)
-        puts("");
-
-    else
     {
-        char *longest_str = argv[1];
-        for (int i = 1; i < argc; i++)
-        {
-            if (strlen(argv[1]) < strlen(argv[i]))
-            {
-                longest_str = argv[i];
-            }
-        }
-        puts(longest_str);
+        longest_str = "";
     }
+    for (int i = 1; i < argc; i++)
+    {
+        if (strlen(argv[1]) < strlen(argv[i]))
+        {
+            longest_str = argv[i];
+        }
+    }
+    puts(longest_str);
     return EXIT_SUCCESS;
 }
